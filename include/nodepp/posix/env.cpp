eca065f8106acf1294900bb9422b53e03b9ec656
edbc <edbc>
2025-02-27 23:22:26 -0400

    update

--- include/nodepp/posix/env.cpp
+++ include/nodepp/posix/env.cpp
@@ -11,50 +11,33 @@
 
 #pragma once
 #include <unistd.h>
-
-/*────────────────────────────────────────────────────────────────────────────*/
-
-namespace nodepp { namespace { 
-    
-    int SET( const string_t& name, const string_t& value ){ return setenv( name.c_str(), value.c_str(), 1 ); }
-
-    string_t GET( const string_t& name ){ return getenv( name.c_str() ); } 
-
-    int DEL( const string_t& name ){ return unsetenv( name.c_str() ); }
-
-  //int CLEAR(){ return clearenv(); }
-
-}}
-
 /*────────────────────────────────────────────────────────────────────────────*/
 
 namespace nodepp { namespace process {
 
     namespace env {
 
-        int set( const string_t& name, const string_t& value ){ return nodepp::SET( name, value ); }
+        int set( const string_t& name, const string_t& value ){ return setenv( name.c_str(), value.c_str(), 1 ); }
 
-        string_t get( const string_t& name ){ return nodepp::GET( name ); }
+        string_t get( const string_t& name ){ return getenv( name.c_str() ); }
 
-        int remove( const string_t& name ){ return nodepp::DEL( name );  } 
-
-      //int clear(){ return nodepp::CLEAR(); }
+        int remove( const string_t& name ){ return unsetenv( name.c_str() ); }
 
         int init( const string_t& path ){
-                
-            FILE* v = fopen( path.c_str(), "r" ); 
+
+            FILE* v = fopen( path.c_str(), "r" );
             string_t s; bool nr = 0; bool pr = 0;
 
-            if( v == nullptr ){ 
+            if( v == nullptr ){
                _ERROR( "such file of directory not found" );
-                return -1; 
+                return -1;
             }
-            
-            array_t<string_t> env ( 2 ); 
-            function_t<void> lb([&](){ 
+
+            array_t<string_t> env ( 2 );
+            function_t<void> lb([&](){
                 if( env[0].empty() ){ return; }
                 if( env[1].empty() ){ return; }
-                nodepp::SET( env[0], env[1] ); 
+                nodepp::SET( env[0], env[1] );
             });
 
             while( !feof(v) ){ int c = fgetc( v );
@@ -65,7 +48,7 @@ namespace nodepp { namespace process {
                 if( c=='#' && !pr ){ nr=1; continue; }
                 if( c==';' && !pr ){ nr=1; continue; }
 
-                  if( c=='=' && !pr && !nr ){ env[0]=s; s.clear(); continue; } 
+                  if( c=='=' && !pr && !nr ){ env[0]=s; s.clear(); continue; }
                 elif( c==-1 )               { env[1]=s; s.clear(); lb(); nr=0; break; }
                 elif( c=='\n'&& !pr )       { env[1]=s; s.clear(); lb(); nr=0; continue; }
 
@@ -74,17 +57,17 @@ namespace nodepp { namespace process {
         };
 
     }
-    
+
     /*─······································································─*/
 
     bool  is_child(){ return !env::get("CHILD").empty(); }
 
     bool is_parent(){ return  env::get("CHILD").empty(); }
-    
+
     string_t shell(){ return env::get("SHELL"); }
 
     string_t  home(){ return env::get("HOME"); }
-    
+
 }}
 
 /*────────────────────────────────────────────────────────────────────────────*/
--- include/nodepp/windows/env.cpp
+++ include/nodepp/windows/env.cpp
@@ -14,59 +14,35 @@
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
-namespace nodepp { namespace { 
+namespace nodepp { namespace process { namespace env {
 
-    /*
-    int CLEAR(){ LPCH environmentStrings = GetEnvironmentStrings();
-        for( LPCTSTR currentVariable = environmentStrings; *currentVariable; currentVariable += lstrlen(currentVariable) + 1 ){
-            SetEnvironmentVariableA( currentVariable, nullptr );
-        }   FreeEnvironmentStrings( environmentStrings ); return 0;
-    }
-    */
+        string_t get( const string_t& name ){ ptr_t<char> buffer ( UNBFF_SIZE );
+            auto x = GetEnvironmentVariableA( name.c_str(), &buffer, buffer.size() );
+            return string_t( &buffer, (ulong) x );
+        }
 
-    string_t GET( const string_t& name ){ ptr_t<char> buffer ( UNBFF_SIZE );
-        auto x = GetEnvironmentVariableA( name.c_str(), &buffer, buffer.size() );
-        return string_t( &buffer, (ulong) x );
-    } 
-    
-    int SET( const string_t& name, const string_t& value ){ 
-        return SetEnvironmentVariableA( name.c_str(), value.c_str() );
-    }
+        int set( const string_t& name, const string_t& value ){
+            return SetEnvironmentVariableA( name.c_str(), value.c_str() );
+        }
 
-    int DEL( const string_t& name ){
-        return SetEnvironmentVariableA( name.c_str(), nullptr ); 
-    }
-
-}}
-
-/*────────────────────────────────────────────────────────────────────────────*/
-
-namespace nodepp { namespace process {
-
-    namespace env {
-
-        int set( const string_t& name, const string_t& value ){ return nodepp::SET( name, value ); }
-
-        string_t get( const string_t& name ){ return nodepp::GET( name ); }
-
-        int remove( const string_t& name ){ return nodepp::DEL( name );  } 
-
-      //int clear(){ return nodepp::CLEAR(); }
+        int remove( const string_t& name ){
+            return SetEnvironmentVariableA( name.c_str(), nullptr );
+        }
 
         int init( const string_t& path ){
-                
-            FILE* v = fopen( path.c_str(), "r" ); 
+
+            FILE* v = fopen( path.c_str(), "r" );
             string_t s; bool nr = 0; bool pr = 0;
 
-            if( v == nullptr ){ 
+            if( v == nullptr ){
                _ERROR( "such file of directory not found" );
-                return -1; 
+                return -1;
             }
-            
-            array_t<string_t> env ( 2 ); 
-            function_t<void> lb([&](){ 
+
+            array_t<string_t> env ( 2 );
+            function_t<void> lb([&](){
                 if( !env[0].empty() && !env[1].empty() )
-                     nodepp::SET( env[0], env[1] ); 
+                     nodepp::SET( env[0], env[1] );
             });
 
             while( !feof(v) ){ int c = fgetc( v );
@@ -77,7 +53,7 @@ namespace nodepp { namespace process {
                 if( c=='#' && !pr ){ nr=1; continue; }
                 if( c==';' && !pr ){ nr=1; continue; }
 
-                  if( c=='=' && !pr && !nr ){ env[0]=s; s.clear(); continue; } 
+                  if( c=='=' && !pr && !nr ){ env[0]=s; s.clear(); continue; }
                 elif( c==-1 )               { env[1]=s; s.clear(); lb(); nr=0; break; }
                 elif( c=='\n'&& !pr )       { env[1]=s; s.clear(); lb(); nr=0; continue; }
 
@@ -85,18 +61,16 @@ namespace nodepp { namespace process {
             }   fclose(v); return  1;
         };
 
-    }
-    
-    /*─······································································─*/
+}   /*─······································································─*/
 
     bool  is_child(){ return !env::get("CHILD").empty(); }
 
     bool is_parent(){ return  env::get("CHILD").empty(); }
 
     string_t  home(){ return env::get("USERPROFILE"); }
-    
+
     string_t shell(){ return env::get("COMSPEC"); }
-    
+
 }}
 
 /*────────────────────────────────────────────────────────────────────────────*/
