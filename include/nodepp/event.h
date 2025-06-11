abf064795e8ae0caa5dc3af0a3f9b574bd699981
edbc_repo <becerracenmanueld@gmail.com>
2025-06-11 16:53:28 -0400

    update

--- include/nodepp/event.h
+++ include/nodepp/event.h
@@ -14,54 +14,69 @@
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
-namespace nodepp { template< class... A > class event_t { 
+namespace nodepp { template< class... A > class event_t {
 protected:
 
-    using NODE = function_t<bool,A...>; queue_t<NODE> obj;
+    struct NODE {  bool      *out;
+        function_t<bool,A...> clb;
+    };  queue_t<NODE> obj;
 
-public: event_t(){}
-    
+public:
+
+    event_t() noexcept {} ~event_t() noexcept { free(); }
+
     /*─······································································─*/
 
     void* operator()( function_t<void,A...> func ) const noexcept { return on(func); }
-    
+
     /*─······································································─*/
 
     void off( void* address ) const noexcept { process::clear( address ); }
 
     void* once( function_t<void,A...> func ) const noexcept {
         if( MAX_EVENTS!=0 && obj.size()>=MAX_EVENTS ){ return nullptr; }
-        ptr_t<bool> out = new bool(1); obj.push([=]( A... args ){
-            if(*out != 0   ){ func( args... ); } 
-            if( out.null() ){ return false; }
-            *out = 0; return *out;
-        }); return &out;
+        ptr_t<bool> out = new bool(1); NODE ctx;
+        ctx.out=&out; ctx.clb=([=]( A... args ){
+            if(*out != 0   ){ func(args...); }
+            if( out.null() ){ return false;  } *out = 0; return *out;
+        }); obj.push(ctx); return &out;
     }
 
     void* on( function_t<void,A...> func ) const noexcept {
         if( MAX_EVENTS!=0 && obj.size()>=MAX_EVENTS ){ return nullptr; }
-        ptr_t<bool> out = new bool(1); obj.push([=]( A... args ){
-            if(*out != 0   ){ func( args... ); } 
-            if( out.null() ){ return false; }
-            return *out;
-        }); return &out;
+        ptr_t<bool> out = new bool(1); NODE ctx;
+        ctx.out=&out; ctx.clb=([=]( A... args ){
+            if(*out != 0   ){ func(args...); }
+            if( out.null() ){ return false;  } return *out;
+        }); obj.push(ctx); return &out;
     }
-    
+
     /*─······································································─*/
 
     bool  empty() const noexcept { return obj.empty(); }
     ulong  size() const noexcept { return obj.size (); }
-    void  clear() const noexcept {        obj.clear(); }
-    
+
+    /*─······································································─*/
+
+    void free() const noexcept {
+        auto x=obj.first(); while( x!=nullptr && !obj.empty() ){
+        auto y=x->next; if( *x->data.out==0 ){ obj.erase(x); } x=y; }
+    }
+
+    void clear() const noexcept {
+        auto x=obj.first(); while( x!=nullptr && !obj.empty() ){
+        auto y=x->next; *x->data.out==0; x=y;
+    }}
+
     /*─······································································─*/
 
     void emit( const A&... args ) const noexcept {
         auto x=obj.first(); while( x!=nullptr && !obj.empty() ){
-        auto y=x->next; 
-            if( !x->data( args... ) ){ obj.erase(x); }
-        x = y; }
+        auto y=x->next;
+            if( *x->data.out == 0 )    { x=y; continue; }
+          elif( !x->data.clb(args...) ){ x=y; continue; } x=y; }
     }
-    
+
 };}
 
 /*────────────────────────────────────────────────────────────────────────────*/
--- include/nodepp/wait.h
+++ include/nodepp/wait.h
@@ -17,9 +17,13 @@
 namespace nodepp { template< class T, class... A > class wait_t { 
 protected:
 
-    using NODE = function_t<bool,T,A...>; queue_t<NODE> obj;
+    struct NODE {  bool        *out;
+        function_t<bool,T,A...> clb;
+    };  queue_t<NODE> obj;
 
-public: wait_t(){}
+public:
+
+    wait_t() noexcept {} ~wait_t() noexcept { free(); }
     
     /*─······································································─*/
 
@@ -31,35 +35,48 @@ public: wait_t(){}
 
     void* once( T val, function_t<void,A...> func ) const noexcept {
         if( MAX_EVENTS!=0 && obj.size()>=MAX_EVENTS ){ return nullptr; }
-        ptr_t<bool> out = new bool(1); obj.push([=]( T arg, A... args ){
-            if(*out != 0 && val == arg ){ func( args... ); }
-            if( out.null() ){ return false; }
-            *out = 0; return *out;
-        }); return &out;
+        ptr_t<bool> out = new bool(1); NODE ctx;
+        ctx.out=&out; ctx.clb=([=]( T arg, A... args ){
+            if( val == arg ){ return true;   }
+            if(*out != 0   ){ func(args...); }
+            if( out.null() ){ return false;  } *out = 0; return *out;
+        }); obj.push(ctx); return &out;
     }
 
     void* on( T val, function_t<void,A...> func ) const noexcept {
         if( MAX_EVENTS!=0 && obj.size()>=MAX_EVENTS ){ return nullptr; }
-        ptr_t<bool> out = new bool(1); obj.push([=]( T arg, A... args ){
-            if(*out != 0 && val == arg ){ func( args... ); } 
-            if( out.null() ){ return false; }
-            return *out;
-        }); return &out;
+        ptr_t<bool> out = new bool(1); NODE ctx;
+        ctx.out=&out; ctx.clb=([=]( T arg, A... args ){
+            if( val == arg ){ return true;   }
+            if(*out != 0   ){ func(args...); }
+            if( out.null() ){ return false;  } return *out;
+        }); obj.push(ctx); return &out;
     }
     
     /*─······································································─*/
 
     bool  empty() const noexcept { return obj.empty(); }
     ulong  size() const noexcept { return obj.size (); }
-    void  clear() const noexcept {        obj.clear(); }
+
+    /*─······································································─*/
+
+    void free() const noexcept {
+        auto x=obj.first(); while( x!=nullptr && !obj.empty() ){
+        auto y=x->next; if( *x->data.out==0 ){ obj.erase(x); } x=y; }
+    }
+
+    void clear() const noexcept {
+        auto x=obj.first(); while( x!=nullptr && !obj.empty() ){
+        auto y=x->next; *x->data.out==0; x=y;
+    }}
     
     /*─······································································─*/
 
     void emit( const T& arg, const A&... args ) const noexcept {
         auto x=obj.first(); while( x!=nullptr && !obj.empty() ){
         auto y=x->next; 
-            if( !x->data( arg, args... ) ){ obj.erase(x); }
-        x = y; }
+            if( *x->data.out == 0 )        { x=y; continue; }
+            if( !x->data.clb(arg,args...) ){ x=y; continue; } x=y; }
     }
     
 };}
