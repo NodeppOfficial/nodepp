6cd0d169f68eb90a648dc9fd1d98cca92aeff47c
penguino <becerracenmanueld@gmail.com>
2026-09-04 17:34:25 -0400

    V1.4.6_7 | json.h metaprogramming indexed tags updated [TESTED]

--- include/nodepp/json.h
+++ include/nodepp/json.h
@@ -65,7 +65,7 @@ protected:
 
         if  ( data.empty() || data[x] == ',' ) /*---*/ { return nullptr; }
         elif( data[x] == '"'     ) /*---------------*/ { 
-              return data.slice( x+1, get_next_sec( x, data ) ); }
+              return data.slice( x+1, get_next_sec( x, data) ); }
         elif( data[x] == '{'     ) /*---------------*/ { return parse( data ); }
         elif( data[x] == '['     ) /*---------------*/ { return parse( data ); }
         elif( data.find("false") ) /*---------------*/ { return (bool) 0; }
@@ -132,7 +132,7 @@ public: json_t() noexcept {}
             } elif( str[x] == ']' || str[x] == '}' || str[x] == ')' ){
                 return nullptr;
             } else {
-                if( string::is_space( str[x] ) )
+                if( string::is_space( str[x]) )
                   { continue; } data.push( str[x] );
             }
 
@@ -163,126 +163,118 @@ public: json_t() noexcept {}
 
         switch( obj.get_type_id() ){
 
-            case 0x0001: return string::to_string( obj.as<int>() );                              break;
-            case 0x0002: return string::to_string( obj.as<uint>() );                             break;
-            case 0x0003: return obj.as<bool>() ? "true" : "false";                               break;
-            case 0x0004: return string::format("\"%c\"",obj.as<char>());                         break;
-            case 0x0005: return string::to_string( obj.as<long>() );                             break;
-            case 0x0006: return string::to_string( obj.as<short>() );                            break;
-            case 0x0007: return string::to_string( obj.as<uchar>() );                            break;
-            case 0x0008: return string::to_string( obj.as<llong>() );                            break;
-            case 0x0009: return string::to_string( obj.as<ulong>() );                            break;
-            case 0x000a: return string::to_string( obj.as<ushort>() );                           break;
-            case 0x000b: return string::to_string( obj.as<ullong>() );                           break;
-            case 0x000c: return string::to_string( obj.as<wchar_t>() );                          break;
-            case 0x000d: return string::to_string( obj.as<char16_t>() );                         break;
-            case 0x000e: return string::to_string( obj.as<char32_t>() );                         break;
-            case 0x000f: return string::to_string( obj.as<float>() );                            break;
-            case 0x0010: return string::to_string( obj.as<double>() );                           break;
-            case 0x0011: return string::to_string( obj.as<ldouble>() );                          break;
-            case 0x0012: return regex ::format("\"${0}\"",obj.as<string_t>().get());             break;
+            case 0x0001: return string::to_string( obj.as<int>      () );      break;
+            case 0x0002: return string::to_string( obj.as<uint>     () );      break;
+            case 0x0003: return obj.as<bool>() ? "true" : "false";             break;
+            case 0x0004: return string::format("\"%c\"",obj.as<char>());       break;
+            case 0x0005: return string::to_string( obj.as<long>     () );      break;
+            case 0x0006: return string::to_string( obj.as<short>    () );      break;
+            case 0x0007: return string::to_string( obj.as<uchar>    () );      break;
+            case 0x0008: return string::to_string( obj.as<llong>    () );      break;
+            case 0x0009: return string::to_string( obj.as<ulong>    () );      break;
+            case 0x000A: return string::to_string( obj.as<ushort>   () );      break;
+            case 0x000B: return string::to_string( obj.as<ullong>   () );      break;
+            case 0x000C: return string::to_string( obj.as<wchar_t>  () );      break;
+            case 0x000D: return string::to_string( obj.as<float>    () );      break;
+            case 0x000E: return string::to_string( obj.as<double>   () );      break;
+            case 0x000F: return string::to_string( obj.as<ldouble>  () );      break;
+            case 0x0010: return regex ::format("\"${0}\"",obj.as<string_t>()); break;
 
-            case 0xfA03: do { out.push('[');
+            case 0xFA03: do { out.push('[');
             for( auto &x: obj.as<array_t<bool>>() )
                { out += regex::format("\"${0}\",",x ? "true":"false" ); }
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xfA04: do { out.push('[');
+            case 0xFA04: do { out.push('[');
             for( auto &x: obj.as<array_t<char>>() )
                { out += string::format("\"%c\",", x ); }
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xfA12: do { out.push('[');
+            case 0xFA10: do { out.push('[');
             for( auto &x: obj.as<array_t<string_t>>() )
                { out += regex::format("\"${0}\",", x.get() ); }
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xfA01: return regex::format("[${0}]",obj.as<array_t<int>>()     .join().get()); break;
-            case 0xfA02: return regex::format("[${0}]",obj.as<array_t<uint>>()    .join().get()); break;
-            case 0xfA05: return regex::format("[${0}]",obj.as<array_t<long>>()    .join().get()); break;
-            case 0xfA06: return regex::format("[${0}]",obj.as<array_t<short>>()   .join().get()); break;
-            case 0xfA07: return regex::format("[${0}]",obj.as<array_t<uchar>>()   .join().get()); break;
-            case 0xfA08: return regex::format("[${0}]",obj.as<array_t<llong>>()   .join().get()); break;
-            case 0xfA09: return regex::format("[${0}]",obj.as<array_t<ulong>>()   .join().get()); break;
-            case 0xfA0a: return regex::format("[${0}]",obj.as<array_t<ushort>>()  .join().get()); break;
-            case 0xfA0b: return regex::format("[${0}]",obj.as<array_t<ullong>>()  .join().get()); break;
-            case 0xfA0c: return regex::format("[${0}]",obj.as<array_t<wchar_t>>() .join().get()); break;
-            case 0xfA0d: return regex::format("[${0}]",obj.as<array_t<char16_t>>().join().get()); break;
-            case 0xfA0e: return regex::format("[${0}]",obj.as<array_t<char32_t>>().join().get()); break;
-            case 0xfA0f: return regex::format("[${0}]",obj.as<array_t<float>>()   .join().get()); break;
-            case 0xfA10: return regex::format("[${0}]",obj.as<array_t<double>>()  .join().get()); break;
-            case 0xfA11: return regex::format("[${0}]",obj.as<array_t<ldouble>>() .join().get()); break;
+            case 0xFA01: return regex::format("[${0}]",string::join(obj.as<array_t<int>>    ()) ); break;
+            case 0xFA02: return regex::format("[${0}]",string::join(obj.as<array_t<uint>>   ()) ); break;
+            case 0xFA05: return regex::format("[${0}]",string::join(obj.as<array_t<long>>   ()) ); break;
+            case 0xFA06: return regex::format("[${0}]",string::join(obj.as<array_t<short>>  ()) ); break;
+            case 0xFA07: return regex::format("[${0}]",string::join(obj.as<array_t<uchar>>  ()) ); break;
+            case 0xFA08: return regex::format("[${0}]",string::join(obj.as<array_t<llong>>  ()) ); break;
+            case 0xFA09: return regex::format("[${0}]",string::join(obj.as<array_t<ulong>>  ()) ); break;
+            case 0xFA0A: return regex::format("[${0}]",string::join(obj.as<array_t<ushort>> ()) ); break;
+            case 0xFA0B: return regex::format("[${0}]",string::join(obj.as<array_t<ullong>> ()) ); break;
+            case 0xFA0C: return regex::format("[${0}]",string::join(obj.as<array_t<wchar_t>>()) ); break;
+            case 0xFA0D: return regex::format("[${0}]",string::join(obj.as<array_t<float>>  ()) ); break;
+            case 0xFA0E: return regex::format("[${0}]",string::join(obj.as<array_t<double>> ()) ); break;
+            case 0xFA0F: return regex::format("[${0}]",string::join(obj.as<array_t<ldouble>>()) ); break;
 
-            case 0xf603: do { out.push('[');
+            case 0xF603: do { out.push('[');
             for( auto &x: obj.as<ptr_t<bool>>() )
                { out += regex::format("\"${0}\",",x ? "true":"false" ); }
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xf604: do { out.push('[');
+            case 0xF604: do { out.push('[');
             for( auto &x: obj.as<ptr_t<char>>() )
                { out += string::format("\"%c\",", x ); }
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xf612: do { out.push('[');
+            case 0xF610: do { out.push('[');
             for( auto &x: obj.as<ptr_t<string_t>>() )
                { out += regex::format("\"${0}\",", x.get() ); }
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xf601: return regex::format("[${0}]",array_t<int>(obj.as<ptr_t<int>>())          .join().get()); break;
-            case 0xf602: return regex::format("[${0}]",array_t<uint>(obj.as<ptr_t<uint>>())        .join().get()); break;
-            case 0xf605: return regex::format("[${0}]",array_t<long>(obj.as<ptr_t<long>>())        .join().get()); break;
-            case 0xf606: return regex::format("[${0}]",array_t<short>(obj.as<ptr_t<short>>())      .join().get()); break;
-            case 0xf607: return regex::format("[${0}]",array_t<uchar>(obj.as<ptr_t<uchar>>())      .join().get()); break;
-            case 0xf608: return regex::format("[${0}]",array_t<llong>(obj.as<ptr_t<llong>>())      .join().get()); break;
-            case 0xf609: return regex::format("[${0}]",array_t<ulong>(obj.as<ptr_t<ulong>>())      .join().get()); break;
-            case 0xf60a: return regex::format("[${0}]",array_t<ushort>(obj.as<ptr_t<ushort>>())    .join().get()); break;
-            case 0xf60b: return regex::format("[${0}]",array_t<ullong>(obj.as<ptr_t<ullong>>())    .join().get()); break;
-            case 0xf60c: return regex::format("[${0}]",array_t<wchar_t>(obj.as<ptr_t<wchar_t>>())  .join().get()); break;
-            case 0xf60d: return regex::format("[${0}]",array_t<char16_t>(obj.as<ptr_t<char16_t>>()).join().get()); break;
-            case 0xf60e: return regex::format("[${0}]",array_t<char32_t>(obj.as<ptr_t<char32_t>>()).join().get()); break;
-            case 0xf60f: return regex::format("[${0}]",array_t<float>(obj.as<ptr_t<float>>())      .join().get()); break;
-            case 0xf610: return regex::format("[${0}]",array_t<double>(obj.as<ptr_t<double>>())    .join().get()); break;
-            case 0xf611: return regex::format("[${0}]",array_t<ldouble>(obj.as<ptr_t<ldouble>>())  .join().get()); break;
+            case 0xF601: return regex::format("[${0}]",string::join(obj.as<ptr_t<int>>    ()) ); break;
+            case 0xF602: return regex::format("[${0}]",string::join(obj.as<ptr_t<uint>>   ()) ); break;
+            case 0xF605: return regex::format("[${0}]",string::join(obj.as<ptr_t<long>>   ()) ); break;
+            case 0xF606: return regex::format("[${0}]",string::join(obj.as<ptr_t<short>>  ()) ); break;
+            case 0xF607: return regex::format("[${0}]",string::join(obj.as<ptr_t<uchar>>  ()) ); break;
+            case 0xF608: return regex::format("[${0}]",string::join(obj.as<ptr_t<llong>>  ()) ); break;
+            case 0xF609: return regex::format("[${0}]",string::join(obj.as<ptr_t<ulong>>  ()) ); break;
+            case 0xF60A: return regex::format("[${0}]",string::join(obj.as<ptr_t<ushort>> ()) ); break;
+            case 0xF60B: return regex::format("[${0}]",string::join(obj.as<ptr_t<ullong>> ()) ); break;
+            case 0xF60C: return regex::format("[${0}]",string::join(obj.as<ptr_t<wchar_t>>()) ); break;
+            case 0xF60D: return regex::format("[${0}]",string::join(obj.as<ptr_t<float>>  ()) ); break;
+            case 0xF60E: return regex::format("[${0}]",string::join(obj.as<ptr_t<double>> ()) ); break;
+            case 0xF60F: return regex::format("[${0}]",string::join(obj.as<ptr_t<ldouble>>()) ); break;
 
-            case 0xf803: do { out.push('[');
-            for( auto &x: obj.as<queue_t<bool>>().data() )
-               { out += regex::format("\"${0}\",",x ? "true":"false" ); }
+            case 0xF803: do { out.push('[');
+            obj.as<queue_t<bool>>().map([&]( bool x )
+               { out += regex::format("\"${0}\",",x ? "true":"false" ); });
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xf804: do { out.push('[');
-            for( auto &x: obj.as<queue_t<char>>().data() )
-               { out += string::format("\"%c\",", x ); }
+            case 0xF804: do { out.push('[');
+            obj.as<queue_t<char>>().map([&]( char x )
+               { out += string::format("\"%c\",", x ); });
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xf812: do { out.push('[');
-            for( auto &x: obj.as<queue_t<string_t>>().data() )
-               { out += regex::format("\"${0}\",", x.get() ); }
+            case 0xF810: do { out.push('[');
+            obj.as<queue_t<string_t>>().map([&]( string_t x )
+               { out += regex::format("\"${0}\",", x.get() ); });
             if ( out[ out.size()-1 ] == ',' ){ out.pop(); }
             out.push(']'); } while(0); break;
 
-            case 0xf801: return regex::format("[${0}]",array_t<int>(obj.as<queue_t<int>>().data())          .join().get()); break;
-            case 0xf802: return regex::format("[${0}]",array_t<uint>(obj.as<queue_t<uint>>().data())        .join().get()); break;
-            case 0xf805: return regex::format("[${0}]",array_t<long>(obj.as<queue_t<long>>().data())        .join().get()); break;
-            case 0xf806: return regex::format("[${0}]",array_t<short>(obj.as<queue_t<short>>().data())      .join().get()); break;
-            case 0xf807: return regex::format("[${0}]",array_t<uchar>(obj.as<queue_t<uchar>>().data())      .join().get()); break;
-            case 0xf808: return regex::format("[${0}]",array_t<llong>(obj.as<queue_t<llong>>().data())      .join().get()); break;
-            case 0xf809: return regex::format("[${0}]",array_t<ulong>(obj.as<queue_t<ulong>>().data())      .join().get()); break;
-            case 0xf80a: return regex::format("[${0}]",array_t<ushort>(obj.as<queue_t<ushort>>().data())    .join().get()); break;
-            case 0xf80b: return regex::format("[${0}]",array_t<ullong>(obj.as<queue_t<ullong>>().data())    .join().get()); break;
-            case 0xf80c: return regex::format("[${0}]",array_t<wchar_t>(obj.as<queue_t<wchar_t>>().data())  .join().get()); break;
-            case 0xf80d: return regex::format("[${0}]",array_t<char16_t>(obj.as<queue_t<char16_t>>().data()).join().get()); break;
-            case 0xf80e: return regex::format("[${0}]",array_t<char32_t>(obj.as<queue_t<char32_t>>().data()).join().get()); break;
-            case 0xf80f: return regex::format("[${0}]",array_t<float>(obj.as<queue_t<float>>().data())      .join().get()); break;
-            case 0xf810: return regex::format("[${0}]",array_t<double>(obj.as<queue_t<double>>().data())    .join().get()); break;
-            case 0xf811: return regex::format("[${0}]",array_t<ldouble>(obj.as<queue_t<ldouble>>().data())  .join().get()); break;
+            case 0xF801: return regex::format("[${0}]",string::join(obj.as<queue_t<int>>    ()) ); break;
+            case 0xF802: return regex::format("[${0}]",string::join(obj.as<queue_t<uint>>   ()) ); break;
+            case 0xF805: return regex::format("[${0}]",string::join(obj.as<queue_t<long>>   ()) ); break;
+            case 0xF806: return regex::format("[${0}]",string::join(obj.as<queue_t<short>>  ()) ); break;
+            case 0xF807: return regex::format("[${0}]",string::join(obj.as<queue_t<uchar>>  ()) ); break;
+            case 0xF808: return regex::format("[${0}]",string::join(obj.as<queue_t<llong>>  ()) ); break;
+            case 0xF809: return regex::format("[${0}]",string::join(obj.as<queue_t<ulong>>  ()) ); break;
+            case 0xF80A: return regex::format("[${0}]",string::join(obj.as<queue_t<ushort>> ()) ); break;
+            case 0xF80B: return regex::format("[${0}]",string::join(obj.as<queue_t<ullong>> ()) ); break;
+            case 0xF80C: return regex::format("[${0}]",string::join(obj.as<queue_t<wchar_t>>()) ); break;
+            case 0xF80D: return regex::format("[${0}]",string::join(obj.as<queue_t<float>>  ()) ); break;
+            case 0xF80E: return regex::format("[${0}]",string::join(obj.as<queue_t<double>> ()) ); break;
+            case 0xF80F: return regex::format("[${0}]",string::join(obj.as<queue_t<ldouble>>()) ); break;
 
             default: return "{}"; break;
         }
--- main.cpp
+++ main.cpp
@@ -1,61 +1,17 @@
 #include <nodepp/nodepp.h>
-#include <nodepp/worker.h>
-#include <nodepp/http.h>
+#include <nodepp/json.h>
 
 using namespace nodepp;
 
-void server() {
+void onMain(){
 
-    auto server = http::server([=]( http_t cli ){ 
-
-        console::log( cli.path, cli.get_fd() );
-        file_t file ( "LICENSE", "r" );
-
-        // file.set_buffer_size( 1024 );
+    object_t ppt ({ 
+        { "var", "mojon" },
+        { "vor", ptr_t<string_t>({ "hello world", "mojon" }) }
         
-        cli.write_header( 200, header_t({
-            { "Content-Type"   , "text/html" },
-            { "Transfer-Encoding", "chunked" },
-        //  { "Content-Length", string::to_string( file.size() ) }
-        }));
-        
-        stream::pipe( file, cli );
-
     });
 
-    server.listen( "localhost", 8000, [=]( socket_t server ){
-        console::log("server started at http://localhost:8000");
-    });
-
-}
-
-void client() {
-
-    fetch_t args;
-            args.method  = "GET";
-            args.url     = "http://localhost:8000/";
-            args.headers = header_t({
-                { "Host", url::host(args.url) }
-            });
-
-    http::fetch( args )
-
-    .then([]( http_t cli ){
-        cli.onClose([](){ console::log("closed"); });
-        cli.onData ([]( string_t chunk ){
-            console::log( chunk );
-        }); stream::pipe( cli );
-    })
-
-    .fail([]( except_t err ){
-        console::error( err );
-    });
-
-}
-
-void onMain() {
-
-    worker::add([=](){ server(); process::wait(); return -1; });
-    worker::add([=](){ client(); process::wait(); return -1; });
+    console::log( ppt["var"].as<string_t>() );
+    console::log( json::format( ppt ) );
 
 }
